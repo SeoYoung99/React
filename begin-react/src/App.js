efce40ca9a417696642e576d98c3670b7314c432
Jung seo young <sju06071@gmail.com>
2021-07-14 16:22:22 +0900

    jsx_rules

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import Hello from "./Hello";
+import "./App.css";
 
 function App() {
+  const name = "react";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontsize: 24,
+    padding: "1rem",
+  };
   return (
-    <div>
+    <>
       <Hello />
-    </div>
+      <div style={style}>{name}</div>
+      {/* JSX 내부에 자바스크립트 변수를 보여줄때는 중괄호 사용*/}
+      <div className="gray-box"></div>
+    </>
   );
 }
 
--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
+  //브라우저에 있는 실제 DOM 내부에 리액트 컴포넌트를 렌더링하겠다
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
+  //id가 root인 DOM을 선택
 );
 
 // If you want to start measuring performance in your app, pass a function
