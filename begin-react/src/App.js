e4983849e32cc7e36089102ed76206525e42b33d
Jung seo young <sju06071@gmail.com>
2021-07-14 16:48:26 +0900

    props

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import Hello from "./Hello";
+import Wrapper from "./Wrapper";
 import "./App.css";
 
 function App() {
@@ -12,7 +13,11 @@ function App() {
   };
   return (
     <>
-      <Hello />
+      <Wrapper>
+        <Hello name="react" color="red" />
+        <Hello color="pink" />
+      </Wrapper>
+      {/* Hello라는 컴포넌트에 props전달, Hello컴포넌트에서 인자로 받는다.*/}
       <div style={style}>{name}</div>
       {/* JSX 내부에 자바스크립트 변수를 보여줄때는 중괄호 사용*/}
       <div className="gray-box"></div>
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,9 +1,12 @@
 import React from "react";
 //컴포넌트를 만들때 리액트를 불러온다.
 
-function Hello() {
+function Hello({ color, name }) {
   //함수형 컴포넌트
-  return <div>안녕하세요</div>;
+  return <div style={{ color }}>안녕하세요 {name}</div>;
 }
+Hello.defaultProps = {
+  name: "이름없음",
+};
 export default Hello;
 //Hello라는 컴포넌트를 내보낸다.
