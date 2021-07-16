101dfd92605ca1cfa789db4ca98bee761803ed3d
Jung seo young <sju06071@gmail.com>
2021-07-16 16:05:26 +0900

    add in List ( using useRef )

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import CreateUser from "./CreateUser";
 import UserList from "./UserList";
 
 function App() {
-  return <UserList />;
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    //input태그의 이벤트
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value, //키 값에 해당하는 value 할당
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ]);
+  const nextId = useRef(4); //nextId.current의 기본값 = 4
+  //useRef로 컴포넌트 안에서 조회 및 수정 할 수 있는 변수를 관리
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]); //users배열 복사후 user객체 추가
+    //setUsers(users.concat(user)); //concat 사용
+    setInputs({
+      //입력칸 초기화
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList
+        users={users}
+        /* users라는 props값에 users배열 객체를 넣어줌 */
+      />
+    </>
+  );
 }
 export default App;
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -9,37 +9,12 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
-    },
-    {
-      id: 2,
-      username: "tester",
-      email: "tester@example.com",
-    },
-    {
-      id: 3,
-      username: "liz",
-      email: "liz@example.com",
-    },
-  ];
-
+function UserList({ users }) {
   return (
     <div>
-      {/* <User user={users[0]} /> 
-          <User user={users[1]} />
-          <User user={users[2]} /> */}
-
-      {
-        users.map((user) => (
-          <User user={user} key={user.id} /* 또는 key ={index} */ />
-        ))
-        /* 배열안에 있는 각 원소(user)를 반환하여 새로운 배열을 생성하므로 동적인 배열을 렌더링할 수 있다. */
-      }
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
     </div>
   );
 }
