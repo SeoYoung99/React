9265f5c033aa836d2fb83ae2b9b0a11a5847be38
Jung seo young <sju06071@gmail.com>
2021-07-17 23:44:17 +0900

    Toggle in Lsit

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -22,16 +22,19 @@ function App() {
       id: 1,
       username: "velopert",
       email: "public.velopert@gmail.com",
+      active: true,
     },
     {
       id: 2,
       username: "tester",
       email: "tester@example.com",
+      active: false,
     },
     {
       id: 3,
       username: "liz",
       email: "liz@example.com",
+      active: false,
     },
   ]);
   const nextId = useRef(4); //nextId.current의 기본값 = 4
@@ -56,6 +59,16 @@ function App() {
     //id가 일치하지 않는 원소만 추출해서 새로운 배열을 만든다. (id에 해당하는 원소만 제거)
     setUsers(users.filter((user) => user.id !== id));
   };
+
+  const onToggle = (id) => {
+    //User컴포넌트 클릭 시 넘어가는 user.id를 인자로 받아서
+    setUsers(
+      users.map(
+        (user) => (user.id === id ? { ...user, active: !user.active } : user)
+        //id가 같으면 active값을 반전, 아니면 그대로
+      )
+    );
+  };
   return (
     <>
       <CreateUser
@@ -67,6 +80,7 @@ function App() {
       <UserList
         users={users}
         onRemove={onRemove}
+        onToggle={onToggle}
         /* users라는 props값에 users배열 객체를 넣어줌 */
       />
     </>
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,20 +1,38 @@
 import React from "react";
 
-function User({ user, onRemove }) {
-  //User컴포넌트: 배열의 각 요소를 보여준다
+//User컴포넌트: 배열의 각 요소를 보여준다
+function User({ user, onRemove, onToggle }) {
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: "pointer",
+          //마우스를 올리면 손가락 모양으로 변한다.
+          color: user.active ? "green" : "black",
+          //active가 참이면 초록색, 아니면 검정색
+        }}
+        onClick={() => onToggle(user.id)} //클릭되면 onToggle함수 실행
+      >
+        {user.username}
+      </b>{" "}
+      <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+        //map() 함수는 users라는 배열의 요소를 돌면서
+        //인자로 전달된 함수를 사용하여 처리 된 새로운 결과를 새로운 배열에 담아 반환
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
